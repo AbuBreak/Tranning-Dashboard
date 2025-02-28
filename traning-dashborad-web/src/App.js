ad45be0901b03d0556d6fd0aa894c51e48525e3a
Malik Abu Break <94977176+AbuBreak@users.noreply.github.com>
2025-03-01 02:17:23 +0300

    phase1-UI(sign up ready)

--- traning-dashborad-web/src/App.js
+++ traning-dashborad-web/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Box, Typography, Container, TextField, Button } from '@mui/material';
+
+import CustomAppBar from './components/app-bar';
+
+/**
+ * Main entry point of the React application.
+ * Includes a navbar, a brief description, and a sign-in form.
+ *
+ * @return {JSX.Element} The JSX representation of the App component.
+ */
+
+{/* TODO add logo, simple image and add animation for them with the description below. */ }
+
+{/* TODO start working with sign up page */}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <CustomAppBar />
+
+      {/* Main Container, which will contain page content */}
+      <Container
+        maxWidth="xs"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          minHeight: '90vh',
+          justifyContent: 'center'
+        }}
+      >
+        {/* Brief Description*/}
+        <Typography variant="h5" textAlign="center" sx={{ mb: 3 }}>
+          Welcome to CourseEase – The Ultimate Course Management Platform!
+        </Typography>
+
+        {/* TODO do cusotm components for each element. */}
+
+        {/* Sign-In Card, which have the main entires required from the user as username | password OR signup if not registered. */}
+
+        <Box
+          sx={{
+            p: 3,
+            boxShadow: 3,
+            borderRadius: 2,
+            bgcolor: 'background.paper',
+            width: '100%',
+            textAlign: 'center'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h4" gutterBottom>
+            Sign In
+          </Typography>
+
+          <TextField
+            fullWidth
+            label="Username"
+            variant="outlined"
+            margin="normal"
+          />
+
+          <TextField
+            fullWidth
+            label="Password"
+            variant="outlined"
+            type="password"
+            margin="normal"
+          />
+
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Container>
+      {/* TODO add footer as a custom for whole project. */}
+
+    </>
   );
 }
 
--- traning-dashborad-web/src/reportWebVitals.js
+++ traning-dashborad-web/src/reportWebVitals.js
@@ -1,3 +1,15 @@
+/**
+
+With the reportWebVitals function,
+you can send any of the results to an analytics endpoint to measure and track real user performance on your site.
+
+@param {Function} onPerfEntry - A callback function that receives performance metrics.
+
+Calls the provided function with metrics like CLS, FID, FCP, LCP, and TTFB.
+
+@return {void}
+*/
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
